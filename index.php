249b33b0dab7e685426709a1819792d641b9dddd
Victoria Gibbs <victoriahgibbs@gmail.com>
2025-02-07 23:30:14 -0500

    Changed root index file to redirect

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php header('Location: public/');?>
+<!-- <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//   <meta charset="utf-8">
+//   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//   <title>Grub Builder</title>
+//   <style>
+//     h1 {
+//       font-size: 55px;
+//       margin-top: 3rem;
+//       text-align: center;
+//     }
+//   </style>
+// </head>
+// <body>
+//   <h1>Grub Builder Coming Soon!</h1>
+// </body>
+// </html> -->
+
