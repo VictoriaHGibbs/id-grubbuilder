d73f679b65705b5f38c00943682130da69ba3879
Victoria Gibbs <victoriahgibbs@gmail.com>
2025-02-25 15:18:34 -0500

    added a little css to the headers, very rough styling

--- private/shared/public_header.php
+++ private/shared/public_header.php
@@ -5,36 +5,38 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Grub Builder <?php if (isset($page_title)) {echo '- ' . htmlspecialchars($page_title);} ?></title>
-  <link rel="stylesheet" href="<?php echo url_for('/stylesheets/style.css'); ?>">
+  <link rel="stylesheet" href="<?php echo url_for('/stylesheets/styles.css'); ?>">
 </head>
 
 <body>
 
   <a href="#main-content" id="skip-link">Jump to main content</a>
   <header>
-    <h1><a href="<?php echo url_for('/index.php'); ?>">Grub Builder</a></h1>
-
-    <form action="<?php echo (SHARED_PATH . '/forms/search.php'); ?>" method="post" role="form">
-      <label for="search"></label>
-      <input type="text" id="search" name="search">
-      <input id="search-submit" type="submit" value="Search">
-    </form>
-
-    <ul>
-      <li><a href="<?php echo url_for('/active_record/login.php') ?>">User Login</a></li>
-      <li><a href="<?php echo url_for('/active_record/signup.php') ?>">No account? Sign-up here!</a></li>
-    </ul>
-
-    <nav role="navigation" aria-label="main site">
+    <section id="header-top">
+      <h1><a href="<?php echo url_for('/index.php'); ?>">Grub Builder</a></h1>
+      <form action="<?php echo (SHARED_PATH . '/forms/search.php'); ?>" method="post" role="form">
+        <!-- <label for="search"></label> -->
+        <input type="text" id="search" name="search">
+        <input id="search-submit" type="submit" value="Search">
+      </form>
       <ul>
-        <li><a href="<?php echo url_for('/recipes.php') ?>">All Recipes</a></li>
-        <li><a href="<?php echo url_for('/.php') ?>">Popular</a></li>
-        <li><a href="<?php echo url_for('/.php') ?>">Courses</a></li>
-        <li><a href="<?php echo url_for('/.php') ?>">Diet</a></li>
-        <li><a href="<?php echo url_for('/.php') ?>">Cuisine</a></li>
-        <li><a href="<?php echo url_for('/about.php') ?>">About us</a></li>
+        <li><a href="<?php echo url_for('/active_record/login.php') ?>">User Login</a></li>
+        <li><a href="<?php echo url_for('/active_record/signup.php') ?>">No account? Sign-up here!</a></li>
       </ul>
-    </nav>
+    </section>
+
+    <section id="header-lower">
+      <nav role="navigation" aria-label="main site">
+        <ul>
+          <li><a href="<?php echo url_for('/recipes.php') ?>">All Recipes</a></li>
+          <li><a href="<?php echo url_for('/.php') ?>">Popular</a></li>
+          <li><a href="<?php echo url_for('/.php') ?>">Courses</a></li>
+          <li><a href="<?php echo url_for('/.php') ?>">Diet</a></li>
+          <li><a href="<?php echo url_for('/.php') ?>">Cuisine</a></li>
+          <li><a href="<?php echo url_for('/about.php') ?>">About us</a></li>
+        </ul>
+      </nav>
+    </section>
   </header>
 
   <main role="main" id="main-content" tabindex="-1">
--- private/shared/user_header.php
+++ private/shared/user_header.php
@@ -10,43 +10,42 @@ $id = $_SESSION['user_id'] ?? false;
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Grub Builder <?php if (isset($page_title)) {echo '- ' . htmlspecialchars($page_title);} ?></title>
-  <link rel="stylesheet" href="<?php echo url_for('/stylesheets/style.css'); ?>">
+  <link rel="stylesheet" href="<?php echo url_for('/stylesheets/styles.css'); ?>">
 </head>
 
 <body>
 
   <header>
-    <h1><a href="<?php echo url_for('/active_record/index.php'); ?>">Grub Builder</a></h1>
-
-    <form action="<?php echo (SHARED_PATH . '/forms/search.php'); ?>" method="post" role="form">
-      <label for="search"></label>
-      <input type="text" id="search" name="search">
-      <input id="search-submit" type="submit" value="Search">
-    </form>
+    <section id="header-top">
+      <h1><a href="<?php echo url_for('/active_record/index.php'); ?>">Grub Builder</a></h1>
+      <form action="<?php echo (SHARED_PATH . '/forms/search.php'); ?>" method="post" role="form">
+        <label for="search"></label>
+        <input type="text" id="search" name="search">
+        <input id="search-submit" type="submit" value="Search">
+      </form>
+        <ul>
+          <li><a href="<?php echo url_for('/active_record/users/index.php'); ?>">Manage Users</a></li><!-- will be for admin only  -->
+      
+          <li><a href="<?php echo url_for('/active_record/index.php'); ?>">My Profile</a></li>
+          <li><a href="<?php echo url_for('/active_record/recipes/new.php'); ?>">Add Recipe</a></li>
+          <li><a href="<?php echo url_for('/active_record/logout.php'); ?>">Logout</a></li>
+        </ul>
+    </section>
+    
+    <section id="header-lower">
+      <nav role="navigation" aria-label="main site">
+        <ul>
+          <li><a href="<?php echo url_for('/active_record/recipes/index.php'); ?>">All Recipe list</a></li>
+          <li><a href="<?php echo url_for('/.php') ?>">Popular</a></li>
+          <li><a href="<?php echo url_for('/.php') ?>">Courses</a></li>
+          <li><a href="<?php echo url_for('/.php') ?>">Diet</a></li>
+          <li><a href="<?php echo url_for('/.php') ?>">Cuisine</a></li>
+          <li><a href="<?php echo url_for('/about.php') ?>">About us</a></li>
+        </ul>
+      </nav>
+    </section>
 
     <h2>Welcome back <?php echo h(User::get_username_by_id($id)); ?></h2>
-
-
-    <nav role="navigation" aria-label="">
-      <ul>
-        <li><a href="<?php echo url_for('/active_record/users/index.php'); ?>">Users</a></li><!-- will be for admin only  -->
-        
-        <li><a href="<?php echo url_for('/active_record/index.php'); ?>">My Profile</a></li>
-        <li><a href="<?php echo url_for('/active_record/recipes/new.php'); ?>">Add Recipe</a></li>
-        <li><a href="<?php echo url_for('/active_record/logout.php'); ?>">Logout</a></li>
-      </ul>
-    </nav>
-
-    <nav role="navigation" aria-label="main site">
-      <ul>
-        <li><a href="<?php echo url_for('/active_record/recipes/index.php'); ?>">All Recipe list</a></li>
-        <li><a href="<?php echo url_for('/.php') ?>">Popular</a></li>
-        <li><a href="<?php echo url_for('/.php') ?>">Courses</a></li>
-        <li><a href="<?php echo url_for('/.php') ?>">Diet</a></li>
-        <li><a href="<?php echo url_for('/.php') ?>">Cuisine</a></li>
-        <li><a href="<?php echo url_for('/about.php') ?>">About us</a></li>
-      </ul>
-    </nav>
   </header>
   
   <main role="main" id="main-content" tabindex="-1">
