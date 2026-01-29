912d246239e1e73ce1d50823cc664f389e1d6780
Victoria Gibbs <victoriahgibbs@gmail.com>
2026-01-28 23:10:02 -0500

    moved local project over to laragon

--- private/initialize.php
+++ private/initialize.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 ob_start();
 
@@ -11,7 +11,9 @@ define("SHARED_PATH", PRIVATE_PATH . '/shared');
 $public_end = strpos($_SERVER['SCRIPT_NAME'], '/public') + 7;
 $doc_root = substr($_SERVER['SCRIPT_NAME'], 0, $public_end);
 define("WWW_ROOT", $doc_root);
-define("IMAGE_PATH", dirname(WWW_ROOT, 1) . 'uploads/');
+// define("IMAGE_PATH", dirname(WWW_ROOT, 1) . '/uploads/');
+define("IMAGE_PATH", "/uploads/");
+
 
 
 require_once('db_credentials.php');
@@ -25,16 +27,16 @@ require_once('status_error_functions.php');
 
 // -> All classes in directory
 foreach (glob('classes/*.class.php') as $file) {
-  require_once($file);
+    require_once($file);
 }
 
 // Autoload class definitions
 function my_autoload($class)
 {
-  $class = strtolower($class);
-  if (preg_match('/\A\w+\Z/', $class)) {
-    include('classes/' . $class . '.class.php');
-  }
+    $class = strtolower($class);
+    if (preg_match('/\A\w+\Z/', $class)) {
+        include('classes/' . $class . '.class.php');
+    }
 }
 spl_autoload_register('my_autoload');
 
--- public/index.php
+++ public/index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 require_once('../private/initialize.php');
 
 $current_page = $_GET['page'] ?? 1;
@@ -12,27 +12,27 @@ $recipes = Recipe::find_all_paginated($per_page, $pagination);
 $page_title = 'Home';
 
 if ($session->is_logged_in()) {
-  include(SHARED_PATH . '/user_header.php');
+    include(SHARED_PATH . '/user_header.php');
 } else {
-  include(SHARED_PATH . '/public_header.php');
+    include(SHARED_PATH . '/public_header.php');
 }
 ?>
 
 <section class="styled-container container text-center my-5 p-4 rounded border border-1 border-dark">
-  <h2 class="display-4 fw-bold text-dark ">Welcome to Grub Builder</h2>
-  <p class="lead">Find, customize, and enjoy recipes tailored to your taste and dietary needs.</p>
-  <a href="<?php echo url_for('/recipes.php') ?>" class="btn btn-lg btn-warning border-dark">Explore Recipes</a>
+    <h2 class="display-4 fw-bold text-dark ">Welcome to Grub Builder</h2>
+    <p class="lead">Find, customize, and enjoy recipes tailored to your taste and dietary needs.</p>
+    <a href="<?php echo url_for('/recipes.php') ?>" class="btn btn-lg btn-warning border-dark">Explore Recipes</a>
 </section>
 
 <div class="styled-container container text-center mt-4 p-md-4 p-2 rounded border border-1 border-dark">
-  <?php include(SHARED_PATH . '/recipe_card.php'); ?>
+    <?php include(SHARED_PATH . '/recipe_card.php'); ?>
 </div>
 
 <?php $url = url_for('/index.php');
-  echo $pagination->page_links($url); ?>
+echo $pagination->page_links($url); ?>
 
 <?php if ($session->is_logged_in()) {
-  include(SHARED_PATH . '/user_footer.php');
+    include(SHARED_PATH . '/user_footer.php');
 } else {
-  include(SHARED_PATH . '/public_footer.php');
+    include(SHARED_PATH . '/public_footer.php');
 } ?>
